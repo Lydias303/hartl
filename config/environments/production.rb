6356414979c8e31f2014b8dc392709035d42cb7c
Lydias303 <shaferlydia@yahoo.com>
2015-02-08 14:07:19 -0700

    fixed confg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'still-fortress-8628.herokuapp.com'
-  confg.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port    => '587',
